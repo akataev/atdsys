30dd1dfbe07fdf7166f437733e7831b909d8f0db
akataev <kataev.petrsu@gmail.com>
2015-01-07 20:34:19 +0300

    added lol output (line 23)

--- server.js
+++ server.js
@@ -20,4 +20,5 @@ app.get('/users/:id', user.findById);
 
 app.listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
+    console.log("lol");
 });
