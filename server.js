2a99046cb23a812ce7eaf361cc464314c6da6fca
akataev <kataev.petrsu@gmail.com>
2015-01-07 20:36:29 +0300

    some changes

--- server.js
+++ server.js
@@ -20,5 +20,4 @@ app.get('/users/:id', user.findById);
 
 app.listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
-    console.log("lol");
 });
